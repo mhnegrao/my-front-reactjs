b91a7117bcf4e363d97288b78a40ea5b479f7621
Marcelo Henrique <mhnegrao@gmail.com>
2021-04-28 23:01:53 -0300

    Personalizando home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
+import logo from '../../assets/images/istockphoto.jpg';
 import { Container } from '../../components/container';
 
 function Home() {
@@ -7,7 +7,28 @@ function Home() {
     <div className="App">
       <Container>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Título da páginaaaaa</h1>
+        <h1>Front Chocolates!!!!!</h1>
+        <table>
+          <thead>
+            <td colSpan="2">
+              <h3>Listagem Chocolates</h3>
+            </td>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Marca</td>
+              <td>Foto</td>
+            </tr>
+            <tr>
+              <td>Marca</td>
+              <td>Foto</td>
+            </tr>
+            <tr>
+              <td>Marca</td>
+              <td>Foto</td>
+            </tr>
+          </tbody>
+        </table>
       </Container>
     </div>
   );
